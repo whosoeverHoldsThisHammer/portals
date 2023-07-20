73547d948377e04b5a7bf5134e06c6847f934ae3
whosoeverHoldsThisHammer <andresjfiorito@hotmail.com>
2023-07-20 16:39:37 -0300

    Open, Close, Minimize and Maximize functionality

--- src/index.js
+++ src/index.js
@@ -1,43 +1,97 @@
 var m = require("mithril")
-let portals = []
+
+const portals = {
+    stacked: [],
+    freeRoaming: [],
+    anchored: []
+}
+
+const anchor = id => {
+    let element = portals.stacked[id]
+    portals.stacked.splice(id, 1)
+    portals.anchored.push(element)
+    m.mount(document.getElementById("anchors"), footer)
+}
+
+const expand = id => {
+    let element = portals.anchored[id]
+    portals.anchored.splice(id, 1)
+    portals.stacked.push(element)
+    m.mount(document.getElementById("anchors"), footer)
+}
 
 const button = () => {
     return m("button", {
         "class": "rounded-md bg-slate-900 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-slate-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-slate-900",
         "type": "button",
-        onclick: () => initialize({ name: "Portal", content: "Portal content" },)
+        onclick: () => initialize(
+            {
+                id: portals.stacked.length + 1,
+                title: "Portal",
+                content: "Portal content"
+            },
+        )
     },
-        "New Portal"
+        "Open New Portal"
     )
 }
 
 const topLevel = {
     view: function (vnode) {
-        return m("div", { "class": "absolute w-full" }, [
-            portals.map((element, i) => {
-                return m(nonModalWindow, { idx: i })
+        return m("div", { "class": "absolute top left w-full" }, [
+            portals.stacked.map((element, i) => {
+                return m(nonModalWindow, { content: element, idx: i })
             })
+        ])
+    }
+}
+
+const footer = {
+    view: function (vnode) {
+        return m("div", { "class": "absolute bottom-0 left-0 w-full" }, [
+            m("div", { "class": "p-4 flex items-center space-x-4" }, [
+                portals.anchored.map((element, i) => {
+                    return m(miminizedWindow, { content: element, idx: i })
+                })
+            ])
+        ])
+    }
+}
+
+
+
+const miminizedWindow = {
+    view: function (vnode) {
+        return m("div", { "class": "w-52 flex items-center space-between px-4 py-2.5 rounded-sm bg-slate-900" }, [
+            m("div", { "class": "text-sm font-medium text-white grow truncate" }, "New Portal"),
+            m("div", { "class": "flex space-x-3" }, [
+                m("svg", { "class": "w-5 h-5 text-slate-300 hover:text-slate-100 cursor-pointer", "xmlns": "http://www.w3.org/2000/svg", "viewBox": "0 0 20 20", "fill": "currentColor", onclick: ()=> expand(vnode.attrs.idx)},
+                    m("path", { "fill-rule": "evenodd", "d": "M3.25 3A2.25 2.25 0 001 5.25v9.5A2.25 2.25 0 003.25 17h13.5A2.25 2.25 0 0019 14.75v-9.5A2.25 2.25 0 0016.75 3H3.25zM2.5 9v5.75c0 .414.336.75.75.75h13.5a.75.75 0 00.75-.75V9h-15zM4 5.25a.75.75 0 00-.75.75v.01c0 .414.336.75.75.75h.01a.75.75 0 00.75-.75V6a.75.75 0 00-.75-.75H4zM6.25 6A.75.75 0 017 5.25h.01a.75.75 0 01.75.75v.01a.75.75 0 01-.75.75H7a.75.75 0 01-.75-.75V6zM10 5.25a.75.75 0 00-.75.75v.01c0 .414.336.75.75.75h.01a.75.75 0 00.75-.75V6a.75.75 0 00-.75-.75H10z", "clip-rule": "evenodd" })
+                ),
+                m("svg", { "class": "w-5 h-5 text-slate-300 hover:text-slate-100 cursor-pointer", "xmlns": "http://www.w3.org/2000/svg", "viewBox": "0 0 20 20", "fill": "currentColor", onclick: ()=> portals.anchored.splice(vnode.attrs.idx, 1) },
+                    m("path", { "d": "M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" })
+                )
+            ])
         ])
     }
 }
 
 const nonModalWindow = {
     view: function (vnode) {
-        return m("div", { "class": "absolute top-0 right-0 -translate-x-6 translate-y-6", "style": `z-index: ${vnode.attrs.idx}` }, [
-            m("div", { "class": "w-screen max-w-md bg-white rounded-lg shadow-xl overflow-hidden" }, [
-                //m("div", {"class": "text-base font-semibold leading-6 text-gray-900"}, "New portal opened!")
-                m("div", { "class": "bg-gray-200 px-4 py-4 sm:px-6" },
+        return m("div", { "class": "absolute -translate-x-6 translate-y-6", "style": `z-index: ${vnode.attrs.idx}; top: ${20 * vnode.attrs.idx}px; right: ${20 * vnode.attrs.idx}px` }, [
+            m("div", { "class": "w-screen max-w-xl border border-gray-300 border-opacity-75 bg-white rounded-lg shadow-xl overflow-hidden" }, [
+                m("div", { "class": "bg-gray-100 px-4 py-4 sm:px-6" },
                     [
                         m("div", { "class": "flex items-center justify-between" },
                             [
                                 m("h2", { "class": "text-base font-bold leading-6", "id": "dialogTitle" },
                                     "New Portal"
                                 ),
-                                m("div", {"class": "flex items-center justify-between space-x-4"}, [
-                                    m("svg", {"class":"w-6 h-6 text-slate-500 hover:text-slate-900 cursor-pointer","xmlns":"http://www.w3.org/2000/svg","fill":"none","viewBox":"0 0 24 24","stroke-width":"1.5","stroke":"currentColor"}, 
-                                        m("path", {"stroke-linecap":"round","stroke-linejoin":"round","d":"M19.5 12h-15"})
+                                m("div", { "class": "flex items-center justify-between space-x-4" }, [
+                                    m("svg", { "class": "w-6 h-6 text-slate-500 hover:text-slate-900 cursor-pointer", "xmlns": "http://www.w3.org/2000/svg", "fill": "none", "viewBox": "0 0 24 24", "stroke-width": "1.5", "stroke": "currentColor", onclick: () => anchor(vnode.attrs.idx) },
+                                        m("path", { "stroke-linecap": "round", "stroke-linejoin": "round", "d": "M19.5 12h-15" })
                                     ),
-                                    m("svg", { "class": "w-6 h-6 text-slate-500 hover:text-slate-900 cursor-pointer", "xmlns": "http://www.w3.org/2000/svg", "fill": "none", "viewBox": "0 0 24 24", "stroke-width": "1.5", "stroke": "currentColor" },
+                                    m("svg", { "class": "w-6 h-6 text-slate-500 hover:text-slate-900 cursor-pointer", "xmlns": "http://www.w3.org/2000/svg", "fill": "none", "viewBox": "0 0 24 24", "stroke-width": "1.5", "stroke": "currentColor", onclick: () => portals.stacked.splice(vnode.attrs.idx, 1) },
                                         m("path", { "stroke-linecap": "round", "stroke-linejoin": "round", "d": "M6 18L18 6M6 6l12 12" })
                                     )
                                 ])
@@ -45,14 +99,14 @@ const nonModalWindow = {
                         ),
                     ]
                 ),
-                m("div", {"class": "p-4" }, "{Title: 'New Portal'}")
+                m("div", { "class": "p-4" }, "{Title: 'New Portal'}")
             ])
         ])
     }
 }
 
 const initialize = (p) => {
-    portals.push(p)
+    portals.stacked.push(p)
     m.mount(document.getElementById("portals"), topLevel)
 }
 
